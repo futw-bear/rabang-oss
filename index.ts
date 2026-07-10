f7b57b3be488fe390e306e22014aa41c68bb327f
J. Dragon <j.dragon@chivincent.net>
2026-07-11 03:33:19 +0800

    feat: create a simple http server with bun

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+const port = Number(Bun.env.PORT ?? 3000);
+
+const server = Bun.serve({
+  port,
+  fetch(request) {
+    const url = new URL(request.url);
+
+    if (request.method === "GET" && url.pathname === "/") {
+      return new Response(null, { status: 204 });
+    }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`HTTP server listening on ${server.url}`);
